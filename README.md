3d162341d4c740ef31e78575c2ba0f4267351637
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2010-02-15 10:46:08 +0100

    added readme

---
+++ README.md
@@ -0,0 +1,42 @@
+Sinatra::ConfigFile
+=======================
+
+Adds YAML config file support to [Sinatra](http://sinatrarb.com).
+
+Config files are expected to represent hashes. When parsing such a config file it will use set to store that value,
+ignoring those directly defined in the app (not those defined by the class it inherits from, i.e. Sinatra::Base or
+Sinatra::BigBand).
+
+BigBand
+-------
+
+Sinatra::ConfigFile is part of the [BigBand](http://github.com/rkh/big_band) stack.
+Check it out if you are looking for other fancy Sinatra extensions.
+
+Example
+-------
+
+    require "sinatra"
+    require "sinatra/config_file"
+
+    configure do
+      set :foo, "bar"
+      config_file "settings.yml"                 # general settings
+      config_file "#{environment}.settings.yml"  # environment specific settings
+      foo # => "bar" (no matter what you put in your config file)
+    end
+
+Now you could write in your settings.yml:
+
+    ---
+    server: [thin, webrick] # use only thin or webrick for #run!
+    public: /var/www        # load public files from /var/www
+    port:   8080            # run on port 8080
+    foo: baz
+    database:
+      adapter: sqlite
+
+In you development.settings.yml:
+
+    database:
+      db_file: development.db
--- lib/sinatra/config_file.rb
+++ lib/sinatra/config_file.rb
@@ -3,39 +3,7 @@ require "sinatra/sugar"
 require "yaml"
 
 module Sinatra
-  # Using YAML config files. Config files are expected to represent hashes.
-  # When parsing such a config file it will use set to store that value, ignoring
-  # those directly defined in the app (not those defined by the class it inherits
-  # from, i.e. Sinatra::Base or BigBand).
-  #
-  # Example:
-  #
-  #   require "sinatra"
-  #   require "sinatra/config_file"
-  #
-  #   configure do
-  #     set :foo, "bar"
-  #     config_file "settings.yml"                 # general settings
-  #     config_file "#{environment}.settings.yml"  # environment specific settings
-  #     foo # => "bar" (no matter what you put in your config file)
-  #   end
-  #
-  # Now you could write in your settings.yml:
-  #
-  #   ---
-  #   server: [thin, webrick] # use only thin or webrick for #run!
-  #   public: /var/www        # load public files from /var/www
-  #   port:   8080            # run on port 8080
-  #   foo: baz
-  #   database:
-  #     adapter: sqlite
-  #
-  # In you development.settings.yml:
-  #
-  #   database:
-  #     db_file: development.db
   module ConfigFile
-
     def self.registered(klass)
       klass.register Sugar
     end
@@ -47,7 +15,6 @@ module Sinatra
         warn "WARNING: could not load config file #{pattern}" if files.empty?
       end
     end
-
   end
 
   register ConfigFile
